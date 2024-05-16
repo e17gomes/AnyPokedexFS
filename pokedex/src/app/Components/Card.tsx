0ed209f12ec1da09465b6ab00f45ff225b4329d3
e17gomesdev <e17gomes@users.noreply.github.com>
2024-05-15 21:32:48 -0300

    Stats and Footer add

--- pokedex/src/app/Components/Card.tsx
+++ pokedex/src/app/Components/Card.tsx
@@ -3,15 +3,18 @@ import axios from "axios";
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { useState, useEffect, ChangeEvent } from "react";
+import { CircleArrowLeft, CircleArrowRight, Search } from "lucide-react";
 
 function GetPokes() {
   const [count, setCount] = useState(1);
   const [poke, setPoke] = useState('')
-  const [namePoke, setnamePoke] = useState(''); //desconsidera
-  const [imgPoke, setImgPoke] = useState(''); //desconsidera
-  const [typeP, setTypeP] = useState(''); //desconsidera
+  const [namePoke, setnamePoke] = useState(''); //
+  const [imgPoke, setImgPoke] = useState(''); //
+  const [typeP, setTypeP] = useState(''); //
   const [SecondaryTypes, setSecondaryTypes] = useState<{ type: { name: string } }[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [pokemonStats, setPokemonStats] = useState<any[]>([]);
+
 
   useEffect(
     //call the function getPoke 
@@ -43,7 +46,7 @@ function GetPokes() {
       parseInt(response.data.id)
 
       setCount(response.data.id)
-
+      setPokemonStats(response.data.stats);
       setImgPoke(response.data.sprites.front_default);
       setnamePoke(response.data.name);
       setTypeP(response.data.types[0].type.name)
@@ -69,26 +72,28 @@ function GetPokes() {
         // On Load
         <div className="relative">
           <div className="flex items-center justify-center h-64">
-            <div className="w-12 h-12 border-4 border-gray-200 border-t-gray-900 rounded-full animate-spin"></div>
+            <div className="w-12 h-12 border-4 border-gray-200 border-t-gray-900 rounded-full animate-spin">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="" className="animate-spin w-24 " />
+            </div>
           </div>
         </div>
       ) : (
         // Load Complete
         <section className="space-y-5 m-auto mt-0">
-          <h1 className="text-6xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-t from-red-600 to-neutral-200 py-4 underline decoration-red-200 decoration-wavy decoration-from-font">Welcome to AnyDex</h1>
+          <h1 className="text-6xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-t from-red-600 to-neutral-200 py-5">Welcome to AnyDex</h1>
   
-          <form onSubmit={handleSearch} className="bg-gray-200 rounded p-2 text-center w-fit m-auto">
+          <form onSubmit={handleSearch} className="bg-gray-200 rounded p-2 text-center w-fit m-auto flex items-center">
             <input className="bg-inherit outline-none dark:text-gray-700" placeholder="insert here a pokemon" type="search" onChange={handleGetVal} />
-            <button onClick={getPoke}>🍳</button>
+            <button onClick={getPoke}><Search className="w-5" /></button>
           </form>
   
           <div className="flex items-center justify-center space-x-3">
             <Button variant="outline" onClick={handleCountPrev}>
-              <p className="h-4 w-4 flex items-center">{'<-'}</p>
+             <CircleArrowLeft/>
             </Button>
   
             <Card className="w-full max-w-sm rounded-lg overflow-hidden shadow-lg ">
-              <div className="relative bg-red-200">
+              <div className="relative bg-slate-300">
                 <img
                   alt="Pokemon"
                   className="m-auto pt-10 w-48 h-48 object-cover"
@@ -115,17 +120,21 @@ function GetPokes() {
                   {/* <div className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-medium">Fire</div>
                   <div className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-medium">Flying</div> */}
                 </div>
-                <p className="text-gray-500 text-sm">
-                    <span>HP</span>
-                </p>
+                <ul className="grid grid-cols-2">
+        {pokemonStats.map((stat, index) => (
+          <li key={index}>
+            {primeiraLetraMaiuscula(stat.stat.name)}: {stat.base_stat}
+          </li>
+        ))}
+      </ul>
               </div>
             </Card>
   
             <Button variant="outline" onClick={handleCountNext}>
-              <p className="h-4 w-4 flex items-center">{'->'}</p>
+              <CircleArrowRight/>
             </Button>
           </div>
-        </section>
+        </section> 
       )}
     </>
   );
--- pokedex/src/app/Components/NavBar.tsx
+++ pokedex/src/app/Components/NavBar.tsx
@@ -1,27 +1,23 @@
 import Link from "next/link";
-import ModeToggle from "./Toggle"
+import ModeToggle from "./Toggle";
 
 export default function Nav() {
   return (
-    <nav className="flex h-14 w-full items-center justify-between px-4  md:px-6 shadow dark:shadow-slate-700 ">
-      <nav className="space-x-10 flex items-center">
-        <Link className="text-4xl font-bold flex relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-red-500 to-neutral-200 py-8 items-center " href="/">
+    <nav className="flex h-14 w-full items-center justify-between px-4 md:px-6 shadow dark:shadow-slate-700 sticky top-0 bg-white dark:bg-black  z-50">
+      <nav className="space-x-10 flex items-center ">
+        <Link className="text-4xl font-bold flex relative z-20 bg-clip-text text-transparent bg-gradient-to-t from-red-500 to-neutral-200 py-8 items-center decoration-red-200  underline decoration-wavy decoration-from-font " href="/">
           AnyDex
-          <img className="absolute z-50 left-[6.9rem] mt-2" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="AnyDex" />
+          <img className="absolute left-[6.9rem] mt-2" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="AnyDex" />
         </Link>
-        <Link className="font-medium text-sm leading-none animate-pulse" href="/convert">
+        <Link className=" animate-pulse" href="https://pokeapi.co/" target="_blank">
           API
         </Link>
       </nav>
 
-    <div className="space-x-5">
-      <Link href={'/'}>
-        About 
-      </Link>
-      <ModeToggle />
-    </div>
+      <div className="space-x-5">
+    
+        <ModeToggle />
+      </div>
     </nav>
-
-  )
-
+  );
 }
