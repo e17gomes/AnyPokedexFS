7d8fa5acacd3f7ad36aa087b0b3b903519a68d74
e17gomesdev <e17gomes@users.noreply.github.com>
2024-05-17 22:33:07 -0300

    Colors on types

--- pokedex/src/app/Components/Card.tsx
+++ pokedex/src/app/Components/Card.tsx
@@ -4,18 +4,24 @@ import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { useState, useEffect, ChangeEvent } from "react";
 import { CircleArrowLeft, CircleArrowRight, Search } from "lucide-react";
+import { colorsType } from "./colorType";
 
 function GetPokes() {
   const [count, setCount] = useState(1);
   const [poke, setPoke] = useState('')
   const [namePoke, setnamePoke] = useState(''); //
   const [imgPoke, setImgPoke] = useState(''); //
-  const [typeP, setTypeP] = useState(''); //
-  const [SecondaryTypes, setSecondaryTypes] = useState<{ type: { name: string } }[]>([]);
+  const [Types, setTypes] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [pokemonStats, setPokemonStats] = useState<any[]>([]);
 
-
+  {
+    Types.map((stypes: { type: { name: string } }, index: number) => {
+      // Acesse a cor correspondente ao tipo atual de Pokémon diretamente do objeto colorsType
+      const backgroundColor = colorsType[stypes.type.name.toLowerCase()] || 'bg-gray-400';
+      // Restante do seu código...
+    })
+  }
   useEffect(
     //call the function getPoke 
     () => {
@@ -36,6 +42,7 @@ function GetPokes() {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
+
   const getPoke = async () => {
     setIsLoading(true)
 
@@ -49,22 +56,21 @@ function GetPokes() {
       setPokemonStats(response.data.stats);
       setImgPoke(response.data.sprites.front_default);
       setnamePoke(response.data.name);
-      setTypeP(response.data.types[0].type.name)
-      setSecondaryTypes(response.data.types)
+      setTypes(response.data.types)
+      console.log(response.data.stats)
 
     } catch (error) {
       console.error("Erro ao obter Pokémon:", error);
     } finally {
-      // setTimeout(() => {
-      //   //testando aqui
-      // }, 700);
-      setIsLoading(false)
+      setTimeout(() => {
+        //testando aqui
+        setIsLoading(false)
+      }, 700);
       setPoke('')
 
     }
   };
 
-  
 
   return (
     <>
@@ -73,7 +79,7 @@ function GetPokes() {
         <div className="relative">
           <div className="flex items-center justify-center h-64">
             <div className="w-12 h-12 border-4 border-gray-200 border-t-gray-900 rounded-full animate-spin">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="" className="animate-spin w-24 " />
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="" className="animate-spin w-24 " />
             </div>
           </div>
         </div>
@@ -81,19 +87,19 @@ function GetPokes() {
         // Load Complete
         <section className="space-y-5 m-auto mt-0">
           <h1 className="text-6xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-t from-red-600 to-neutral-200 py-5">Welcome to AnyDex</h1>
-  
+
           <form onSubmit={handleSearch} className="bg-gray-200 rounded p-2 text-center w-fit m-auto flex items-center">
             <input className="bg-inherit outline-none dark:text-gray-700" placeholder="insert here a pokemon" type="search" onChange={handleGetVal} />
-            <button onClick={getPoke}><Search className="w-5" /></button>
+            <button onClick={getPoke}><Search className="w-5 text-gray" /></button>
           </form>
-  
+
           <div className="flex items-center justify-center space-x-3">
             <Button variant="outline" onClick={handleCountPrev}>
-             <CircleArrowLeft/>
+              <CircleArrowLeft />
             </Button>
-  
+
             <Card className="w-full max-w-sm rounded-lg overflow-hidden shadow-lg ">
-              <div className="relative bg-slate-300">
+              <div className="relative bg-gray-50">
                 <img
                   alt="Pokemon"
                   className="m-auto pt-10 w-48 h-48 object-cover"
@@ -104,41 +110,41 @@ function GetPokes() {
                     objectFit: "cover",
                   }}
                   width={300}
-                
+
                 />
-                <div className="absolute top-4 left-4 bg-gray-900 text-white px-3 py-1 rounded-full text-xs font-medium">
-                  {primeiraLetraMaiuscula(typeP)}
+                <div className={`absolute top-4 left-4  text-white px-3 py-1 rounded-full text-xs font-medium`}>
+                  {Types.map((stypes: any, index: number) => {
+                    const backgroundColor = colorsType[stypes.type.name.toLowerCase()] || 'bg-gray-400';
+                    return (<div className={`${backgroundColor} text-gray-50 px-3 py-1 rounded-full text-xs font-medium m-1 shadow shadow-black`} key={index}>{primeiraLetraMaiuscula(stypes.type.name)}</div>)
+                  })}
                 </div>
               </div>
-  
-              <div className="p-6">
-                <h3 className="text-xl font-bold mb-2">{primeiraLetraMaiuscula(namePoke)}</h3>
-                <div className="flex items-center space-x-2 mb-4">
-                  {SecondaryTypes.map((stypes:any,index:number)=>{
-                    return (<div className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-medium" key={index}>{primeiraLetraMaiuscula(stypes.type.name)}</div>)
-                  })}
-                  {/* <div className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-medium">Fire</div>
-                  <div className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-medium">Flying</div> */}
-                </div>
+
+              <div className="p-4">
+                <h3 className={`text-2xl font-bold mb-2 shadow-inner shadow-slate-300 rounded-full text-center ${Types[0] ? colorsType[Types[0].type.name.toLowerCase()] : 'bg-gray-400'}`}>
+                  {primeiraLetraMaiuscula(namePoke)}
+                </h3>
                 <ul className="grid grid-cols-2">
-        {pokemonStats.map((stat, index) => (
-          <li key={index}>
-            {primeiraLetraMaiuscula(stat.stat.name)}: {stat.base_stat}
-          </li>
-        ))}
-      </ul>
+                  {pokemonStats.map((stat, index) => (
+                    <li key={index}>
+                      {primeiraLetraMaiuscula(stat.stat.name)}: {stat.base_stat}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </Card>
-  
+
             <Button variant="outline" onClick={handleCountNext}>
-              <CircleArrowRight/>
+              <CircleArrowRight />
             </Button>
           </div>
-        </section> 
+        </section>
       )}
+
+
     </>
   );
-  
+
 }
 
 export default GetPokes
---
+++ pokedex/src/app/Components/colorType.ts
@@ -0,0 +1,25 @@
+export interface ColorType {
+    [key: string]: string;
+  }
+  
+  export const colorsType: ColorType = {
+    normal: 'bg-gray-400',
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    grass: 'bg-green-500',
+    electric: 'bg-yellow-500',
+    ice: 'bg-blue-300',
+    fighting: 'bg-red-700',
+    poison: 'bg-purple-500',
+    ground: 'bg-yellow-600',
+    flying: 'bg-blue-200',
+    psychic: 'bg-pink-500',
+    bug: 'bg-green-600',
+    rock: 'bg-gray-600',
+    ghost: 'bg-purple-700',
+    dragon: 'bg-purple-800',
+    dark: 'bg-gray-800',
+    steel: 'bg-gray-500',
+    fairy: 'bg-pink-300',
+  };
+  
